bd09a84fb66455576d4b4936e307275d12137bf9
LeeeChenn <lee.chen.art@gmail.com>
2025-05-24 16:47:16 +0800

    build set

--- vite.config.js
+++ vite.config.js
@@ -16,4 +16,11 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  }
 })
